8d7e9b9920874ccbef49bdcddf40f63ae6405ea6
leely8 <leely9684@sina.com.cn>
2016-03-21 22:54:11 +0800

    打算

---
+++ NewProject5/NewProject5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewProject5
+//
+//  Created by leely8 on 21/3/16.
+//  Copyright © 2016年 leely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
